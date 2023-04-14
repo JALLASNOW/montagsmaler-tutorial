2e9d2efe177b5908db880ea0af1c87088964c60b
Leon Cleven <diegimper@gmail.com>
2023-04-14 22:33:03 +0200

    initialer Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Hallo");
+
+const wordEl = document.querySelector(".word");
+const oldWordsEl = document.querySelector(".old-words");
+
+//Wortliste
+let words = [];
+let currentWord = "";
+let previousWords = [];
+
+fetch("words.txt")
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (text) {
+    words = text.split(",");
+  });
+
+//Das passiert, wenn du den Button drückst
+function onClick() {
+    if(currentWord) {
+        //Wenn wir gerade noch ein Wort anzeigen, so fügen wir dieses den vorherigen Wörtern hinzu
+        previousWords.push(currentWord);
+        //Danach aktualisieren wir unsere Website, um den neuen Wert anzuzeigen
+        oldWordsEl.innerHTML = previousWords.join(", ");
+    }
+  //Neues Wort generieren und als aktuelles Wort speichern
+  currentWord = getRandomWord();
+  //Danach aktualisieren wir unsere Website, um das neue Wort anzuzeigen
+  wordEl.innerHTML = currentWord;
+}
+
+function getRandomNumber(maxNumber) {
+  return Math.floor(Math.random() * maxNumber);
+}
+
+function getRandomWord() {
+  return words[getRandomNumber(words.length)];
+}
